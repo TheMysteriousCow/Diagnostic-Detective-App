3eea16af0cb778c2c62f8ea2e65425884d63d2a6
TheMysteriousCow <meissnao@students.zhaw.ch>
2026-02-25 10:21:48 +0100

    Email Adresse hinzugefügt

--- views/home.py
+++ views/home.py
@@ -6,8 +6,8 @@ st.title("Meine erste Streamlit App")
 
 """
 Diese App wurde von folgenden Personen entwickelt:
-- Anna Müller (anmu@zhaw.ch)
-- Peter Meier (peme@zhaw.ch)
+- Naomi Meisser (meissnao@students.zhaw.ch)
+- Sina Bösiger (boesisin@students.zhaw.ch)
 
 Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
 
