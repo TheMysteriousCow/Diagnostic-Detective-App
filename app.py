4f9a6cb10be39a4594e09e4add0f8afed8451450
TheMysteriousCow <meissnao@students.zhaw.ch>
2026-03-04 10:19:02 +0100

    Addieren Übung 2

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ import streamlit as st
 
 st.set_page_config(page_title="Meine App", page_icon=":material/home:")
 
-pg_home = st.Page("views/home.py", title="Home", icon=":material/home:", default=True)
-pg_second = st.Page("views/unterseite_a.py", title="Unterseite A", icon=":material/info:")
+pg_home = st.Page("views/home.py", title="Home", icon=":material/home:")
+pg_second = st.Page("views/addieren_rechner.py", title="Unterseite A")
 
 pg = st.navigation([pg_home, pg_second])
 pg.run()
+
