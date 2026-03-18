af6facd985dec776e8e6be79acbce119469893c1
TheCooolCow <boesisin@students.zhaw.ch>
2026-03-18 15:27:07 +0100

    Revise caffeine effects description in home.py
    Updated caffeine metabolism explanation and added half-life clarification.

--- views/home.py
+++ views/home.py
@@ -3,10 +3,8 @@ import streamlit as st
 st.title("Meine erste Streamlit App")
 
 """
-Nach dem Konsum von Kaffee, Tee oder Energydrinks gelangt Koffein schnell über den Magen-Darm-Trakt ins Blut und erreicht bereits nach 15 bis 45 Minuten 
-seine maximale Wirkung im zentralen Nervensystem. Der Abbau erfolgt anschließend in der Leber, vor allem durch das Enzym CYP1A2, das Koffein in verschiedene 
-Zwischenprodukte umwandelt.Wie schnell dieser Prozess abläuft, ist individuell unterschiedlich und hängt unter anderem von Genetik, Alter, Lebensstil und 
-Medikamenten ab. Im Durchschnitt beträgt die Halbwertszeit etwa 3 bis 5 Stunden, für unsere App wird daher mit 5 Stunden gerechnet. Dadurch kann Koffein noch lange 
+Nach dem Konsum von Kaffee, Tee oder Energydrinks gelangt Koffein schnell über den Magen-Darm-Trakt ins Blut und erreicht bereits nach 15 bis 45 Minuten seine maximale Wirkung im zentralen Nervensystem. Der Abbau erfolgt anschliessend in der Leber, vor allem durch das Enzym CYP1A2, das Koffein in verschiedene 
+Zwischenprodukte umwandelt. Wie schnell dieser Prozess abläuft, ist individuell unterschiedlich und hängt unter anderem von Genetik, Alter, Lebensstil und Medikamenten ab. Im Durchschnitt beträgt die Halbwertszeit (Zeit, in der die Hälfte des aufgenommenen Koffeins abgebaut ist) etwa 3 bis 5 Stunden. Für unsere App wird daher mit 5 Stunden gerechnet. Demnach kann Koffein noch lange 
 nach dem Konsum im Körper wirken und beispielsweise den Schlaf beeinflussen.
 
 Diese App wurde von folgenden Personen entwickelt:
